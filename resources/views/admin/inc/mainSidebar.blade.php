11c9dbc6e84b2a57beb2d5c79be07ef6715b2831
developerazad <developerazad.hstu@gmail.com>
2019-07-24 22:14:27 +0600

    some design and view changed

--- resources/views/admin/inc/mainSidebar.blade.php
+++ resources/views/admin/inc/mainSidebar.blade.php
@@ -7,7 +7,7 @@
                 <img src="{{ URL::TO('assets/admin/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
-                <p>Alexander Pierce</p>
+                <p>Admin</p>
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
@@ -24,7 +24,7 @@
         <!-- /.search form -->
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
-            <li class="header">MAIN NAVIGATION</li>
+            <li class="header">Main Navigation</li>
             <li class="active treeview">
                 <a href="#">
                     <i class="fa fa-dashboard"></i> <span>Dashboard</span>
