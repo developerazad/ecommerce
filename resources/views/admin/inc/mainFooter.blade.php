35b55920ebf89b6926289702e25b88bda61854c6
developerazad <developerazad.hstu@gmail.com>
2019-09-22 20:48:31 +0600

    some minor changes to files

--- resources/views/admin/inc/mainFooter.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="text-center hidden-xs">
-        <strong>Copyright &copy; 2019 <a href="http://developerazad.com">Md. Azharul Islam</a>.</strong> All rights
-        reserved.
-    </div>
-</footer>
