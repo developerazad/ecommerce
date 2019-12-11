5fecf669113c2b47ddd81cd3766a0e6e04d285cc
developerazad <developerazad.hstu@gmail.com>
2019-12-11 20:23:01 +0600

    authentication in admin panle

--- app/Http/Controllers/ModuleLinkController.php
+++ app/Http/Controllers/ModuleLinkController.php
@@ -13,6 +13,12 @@ class ModuleLinkController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $header =[
