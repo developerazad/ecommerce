d14df68db1e6ad50cb742a07d322d57b38bb788e
developerazad <developerazad.hstu@gmail.com>
2019-12-26 23:56:57 +0600

    nothing

--- resources/views/public/inc/header.blade.php
+++ resources/views/public/inc/header.blade.php
@@ -26,8 +26,14 @@
                         @if(Session::get('customer_name'))
                         <li><a href="#"><i class="fa fa-user"></i> Account</a></li>
                         @endif
+
                         <li><a href="{{ url('checkout') }}"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+
+                        @if(Cart::count() > 0)
                         <li><a class="{{ Request::is('carts')?'active':'' }}" href="{{ url('carts') }}"><i class="fa fa-shopping-cart"></i> Cart <sup>{{ Cart::count() }}</sup></a></li>
+                        @else
+                        @endif
+
                         @if(Session::get('customer_name'))
                         <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();logoutMessage()" role="menuitem" tabindex="-1"><i class="fa fa-unlock"></i> Logout</a></li>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
