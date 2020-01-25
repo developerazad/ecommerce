f68a13f85dc733d41b290fcb15076f32c4cc31ec
developerazad <developerazad.hstu@gmail.com>
2020-01-25 20:34:36 +0600

    module assing

--- resources/views/admin/layouts/accessControl/modulesAssign/index.blade.php
+++ resources/views/admin/layouts/accessControl/modulesAssign/index.blade.php
@@ -6,9 +6,19 @@
         <div class="row">
             <div class="col-xs-12">
 
-                <div class="box">
+                <div class="box box-info">
                     <div class="box-header">
-                        <h3 class="box-title">User list</h3>
+                        <h3 class="box-title">
+                            User Groups
+                        </h3>
+                        <!-- tools box -->
+                        <div class="pull-right box-tools">
+                            <button type="button" class="btn btn-info btn-sm" data-widget="collapse" data-toggle="tooltip" title="" data-original-title="Collapse">
+                                <i class="fa fa-minus"></i></button>
+                            <button type="button" class="btn btn-info btn-sm" data-widget="remove" data-toggle="tooltip" title="" data-original-title="Remove">
+                                <i class="fa fa-times"></i></button>
+                        </div>
+                        <!-- /. tools -->
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
