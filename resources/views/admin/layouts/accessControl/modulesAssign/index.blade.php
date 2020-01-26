c80ab56405c61e4d7c4c920036fa169fbaf0bb05
developerazad <developerazad.hstu@gmail.com>
2020-01-26 23:57:42 +0600

    module assing index

--- resources/views/admin/layouts/accessControl/modulesAssign/index.blade.php
+++ resources/views/admin/layouts/accessControl/modulesAssign/index.blade.php
@@ -15,14 +15,44 @@
                         <div class="pull-right box-tools">
                             <button type="button" class="btn btn-info btn-sm" data-widget="collapse" data-toggle="tooltip" title="" data-original-title="Collapse">
                                 <i class="fa fa-minus"></i></button>
-                            <button type="button" class="btn btn-info btn-sm" data-widget="remove" data-toggle="tooltip" title="" data-original-title="Remove">
-                                <i class="fa fa-times"></i></button>
                         </div>
                         <!-- /. tools -->
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
-
+                        <table class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <td>Link Name</td>
+                                <td>Create</td>
+                                <td>Read</td>
+                                <td>Update</td>
+                                <td>Delete</td>
+                                <td>Status</td>
+                            </tr>
+                            <tr class="bg-info">
+                                <td colspan="6">Access Control</td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>Module</td>
+                                <td><input type="checkbox" class="create-permission"></td>
+                                <td><input type="checkbox"></td>
+                                <td><input type="checkbox"></td>
+                                <td><input type="checkbox"></td>
+                                <td><input type="checkbox"></td>
+                            </tr>
+                            <tr>
+                                <td>Module Link</td>
+                                <td><input type="checkbox"></td>
+                                <td><input type="checkbox"></td>
+                                <td><input type="checkbox"></td>
+                                <td><input type="checkbox"></td>
+                                <td><input type="checkbox"></td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
                     <!-- /.box-body -->
                 </div>
